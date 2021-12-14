c5ac248c005c997b850fe2a979d9cd39bc3dd093
yehanpabasara <95946773+yehanpabasara@users.noreply.github.com>
2021-12-14 18:31:03 +0530

    Update SQ-QA.js

--- SQ-QA.js
+++ SQ-QA.js
@@ -1,5 +1,5 @@
 module.exports = {
-   SEW_QUEEN_OWNER: '94785435462,94785457519', // Ravindu Manoj - Sew Queen Owner
-   SEW_QUEEN_TEAM: '94714366177,94766911891,94711031915,94757222271,94713179802', // Team - SEW QUEEN TEAM 
+   SEW_QUEEN_OWNER: '94704836424', // KING YEHAN AND NADUN - Sew Queen Owner,s
+   SEW_QUEEN_TEAM: '94761276961', // Team - SEW QUEEN TEAM BY (king yehan)
    QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship 
     } 
